349b1b13429040c1a8c38e1b88e64b913fb893d7
José Miguel Moreno <josemmo@pm.me>
2020-10-04 15:03:34 +0200

    Added simple UBL reader test
    - Created UblReaderTest
    - Disabled PHPUnit result caching

---
+++ tests/Readers/UblReaderTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace Tests\Readers;
+
+use Einvoicing\Readers\UblReader;
+use PHPUnit\Framework\TestCase;
+
+final class UblReaderTest extends TestCase {
+    const DOCUMENT_PATH = __DIR__ . "/peppol-example.xml";
+
+    /** @var UblReader */
+    private $reader;
+
+    protected function setUp(): void {
+        $this->reader = new UblReader();
+    }
+
+    public function testCanReadInvoice(): void {
+        $invoice = $this->reader->import(file_get_contents(self::DOCUMENT_PATH));
+        $totals = $invoice->getTotals();
+        $this->assertEquals(1300, $totals->netAmount);
+        $this->assertEquals(1325, $totals->taxExclusiveAmount);
+        $this->assertEquals(331.25, $totals->vatAmount);
+        $this->assertEquals(0, $totals->allowancesAmount);
+        $this->assertEquals(25, $totals->chargesAmount);
+        $this->assertEquals(1656.25, $totals->payableAmount);
+        $this->assertEquals('S', $totals->vatBreakdown[0]->category);
+        $this->assertEquals(25, $totals->vatBreakdown[0]->rate);
+    }
+}
