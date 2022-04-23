b8f50d3e980944c4e4b5bc55d1e9ca19fddd4196
José Miguel Moreno <josemmo@pm.me>
2022-04-23 22:20:45 +0200

    Fixed whitespace in InvoiceTotals
    - Updated InvoiceTotals docblocks

--- src/Models/InvoiceTotals.php
+++ src/Models/InvoiceTotals.php
@@ -12,55 +12,55 @@ class InvoiceTotals {
      */
     public $currency;
 
-    /** 
+    /**
      * Sum of all invoice line net amounts
      * @var float
      */
     public $netAmount = 0;
 
-    /** 
+    /**
      * Sum of all allowances on document level
      * @var float
      */
     public $allowancesAmount = 0;
 
-    /** 
+    /**
      * Sum of all charges on document level
      * @var float
      */
     public $chargesAmount = 0;
 
-    /** 
+    /**
      * Total VAT amount for the invoice
      * @var float
      */
     public $vatAmount = 0;
 
-    /** 
+    /**
      * Invoice total amount without VAT
      * @var float
      */
     public $taxExclusiveAmount = 0;
 
-    /** 
+    /**
      * Invoice total amount with VAT
      * @var float
      */
     public $taxInclusiveAmount = 0;
     
-    /** 
+    /**
      * The sum of amounts which have been paid in advance
      * @var float
      */
     public $paidAmount = 0;
 
-    /** 
+    /**
      * The amount to be added to the invoice total to round the amount to be paid
      * @var float
      */
     public $roundingAmount = 0;
 
-    /** 
+    /**
      * Amount due for payment
      * @var float
      */
